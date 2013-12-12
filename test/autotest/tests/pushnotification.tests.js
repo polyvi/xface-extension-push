849f5284085bba66f015bc598ae830a7ce9c5e43
luoqpolyvi <luoq@polyvi.com>
2013-12-12 14:49:25 +0800

    added jasmine tests unique id

--- test/autotest/tests/pushnotification.tests.js
+++ test/autotest/tests/pushnotification.tests.js
@@ -1,9 +1,9 @@
 describe('PushNotification (xFace.PushNotification)', function () {
-   it("should exist", function() {
+   it("pushnotification.spec.1 should exist", function() {
         expect(xFace.PushNotification).toBeDefined();
     });
 
-    it("should contain proper functions", function() {
+    it("pushnotification.spec.2 should contain proper functions", function() {
         expect(xFace.PushNotification.getDeviceToken).toBeDefined();
         expect(typeof xFace.PushNotification.getDeviceToken).toBe('function');
         expect(xFace.PushNotification.registerOnReceivedListener).toBeDefined();
